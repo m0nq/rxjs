2557d5a68a2d074bf5bb716f2e910b2e8528ec3c
Monk Wellington <forgetmenaught@gmail.com>
2019-08-26 12:51:57 -0700

    Created first observable object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'rxjs';
+  title = 'RxJS';
+  observable;
+
+  ngOnInit() {
+    this.observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    console.log('just before subscribe');
+    this.observable.subscribe({
+      next(x) { console.log('got value ' + x); },
+      error(err) { console.error('something wrong occurred: ' + err); },
+      complete() { console.log('done'); }
+    });
+    console.log('just after subscribe');
+  }
+
+  ngOnDestroy() {
+    this.observable.unsubscribe();
+  }
 }
