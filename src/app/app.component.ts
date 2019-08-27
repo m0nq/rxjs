49703afcbff81bfcef53c515ee7926c1d85a5976
Monk Wellington <forgetmenaught@gmail.com>
2019-08-26 21:48:37 -0700

    Added event observable to dom button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, of } from 'rxjs';
-import { map, switchMap, take } from 'rxjs/operators';
+import { fromEvent, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +8,11 @@ import { map, switchMap, take } from 'rxjs/operators';
 })
 
 export class AppComponent implements OnInit, OnDestroy {
-  title = 'RxJS';
-  mySubject;
 
   ngOnInit() {
-    const numbers = interval(1000);
-    const letters = of('a', 'b', 'c', 'd', 'e');
-
-    letters
-      .pipe(switchMap(x => numbers
-        .pipe(take(5))
-        .pipe(map(i => i + x))))
-      .subscribe(x => console.log(x));
+    fromEvent(document, 'click').subscribe(event => console.log(event.target));
   }
 
   ngOnDestroy() {
-    this.mySubject.unsubscribe();
   }
 }
